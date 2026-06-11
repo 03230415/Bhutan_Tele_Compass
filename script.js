2b969d8c96c6853ad37fa21e6ba58964b113dae1
Yangchey <yangchenom2004@gmail.com>
2026-06-11 10:18:54 +0600

    changes

--- script.js
+++ script.js
@@ -218,7 +218,7 @@ async function handleContact() {
   btn.disabled = true;
 
   try {
-    const res  = await fetch('https://bhutan-tele-compass-derx.onrender.com', {
+    const res  = await fetch('https://bhutan-tele-compass-derx.onrender.com/message', {
       method:  'POST',
       headers: { 'Content-Type': 'application/json' },
       body:    JSON.stringify({ name, email, topic, message })
